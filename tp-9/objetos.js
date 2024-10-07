1cff0add1eb4eb43fa11f21b019b1d4586246682
tomyramirez <166451667+tomyramirez@users.noreply.github.com>
2024-10-07 15:48:16 -0300

    Add files via upload

---
+++ tp-9/objetos.js
@@ -0,0 +1,155 @@
+var personaEjemplo = {
+    "apellido": "Perez",
+    "nombre": "Juan",
+    "edad": 20,
+    "documento": 12345
+};
+
+/**
+ * 01 - crearPersona
+ * 
+ * Recibe
+ * - `nombre`: string, con el nombre.
+ * - `apellido`: string, con el apellido.
+ * - `edad`: numero entero, con la edad de la persona.
+ * - `documento`: numero entero, con el documento de la persona.
+ * 
+ * Retorna: 
+ * - un objeto, representando una persona, con los campos recibidos.
+ * 
+ * Ejemplos:
+ * - crearPersona("Juan", "Pérez", 20, 123456)
+ * {
+ *   nombre: "Juan",
+ *   apellido: "Pérez",
+ *   edad: 20,
+ *   documento: 123456,
+ * }
+ */
+function crearPersona(nombre, apellido, edad, documento) {
+    let objetoPersona = {
+        apellido: apellido,
+        nombre: nombre,
+        edad: edad,
+        documento: documento,
+    };
+    return objetoPersona;
+}
+console.log("resultado crearPersona: ", crearPersona("Juan", "Pérez", 20, 123456));
+
+
+/**
+ * 02 - agregarApodo
+ * 
+ * Recibe
+ * - `persona`: objeto de la forma persona.
+ * - `apodo`: string con el apodo de la persona.
+ * 
+ * Retorna: 
+ * - un objeto, representando a la misma persona recibida, pero con un nuevo campo 'apodo'.
+ */
+function agregarApodo(persona, apodo) {
+    let nuevaPersona = {
+        apellido: persona["apellido"],
+        nombre: persona["nombre"],
+        edad: persona["edad"],
+        documento: persona["documento"],
+        apodo: apodo,
+    }
+    return nuevaPersona;
+}
+console.log("resultado agregarApodo: ", agregarApodo(personaEjemplo, "JuanPe"));
+
+
+/**
+ * 03 - sinDocumento
+ * 
+ * Recibe
+ * - `persona`: objeto de la forma persona.
+ * 
+ * Retorna: 
+ * - un objeto, representando a la misma persona recibida, pero sin el campo documento.
+ */
+function sinDocumento(persona) {
+    let personaSinDocumento = {
+        apellido: persona["apellido"],
+        nombre: persona["nombre"],
+        edad: persona["edad"],
+        documento: persona["documento"],
+    }
+    delete personaSinDocumento.documento
+    return personaSinDocumento
+}
+console.log("resultado sinDocumento: ", sinDocumento(personaEjemplo));
+
+
+/**
+ * 04 - nombreCompletoDePersona
+ * 
+ * Recibe
+ * - `persona`: objeto de la forma persona.
+ * 
+ * Retorna: 
+ * - un string, con el nombre completo de una persona. Asumimos que nombre completo tiene el formato "Apellido, Nombre". Por ejemplo para una persona con nombre "Juan" y apellido "Pérez", el nombre completo sería "Pérez, Juan".
+ */
+function nombreCompletoDePersona(persona) {
+    let nuevoNombre = {
+        apellido: `${persona.apellido}, ${persona.nombre}`,
+        edad: persona["edad"],
+        documento: persona["documento"],
+    }
+    return nuevoNombre;
+}
+console.log("resultado nombreCompletoDePersona: ", nombreCompletoDePersona(personaEjemplo));
+
+
+/**
+ * 05 - felizCumpleaños
+ * 
+ * Recibe
+ * - `persona`: objeto de la forma persona.
+ * 
+ * Retorna: 
+ * - un objeto, representando a la misma persona recibida, pero con un año más. 
+ */
+function felizCumpleaños(persona) {
+    let personaCumpleanios = {...persona};
+    personaCumpleanios.edad += 1;
+    return personaCumpleanios;
+}
+console.log("resultado felizCumpleaños: ", felizCumpleaños(personaEjemplo));
+
+
+/**
+ * 06 - sonLaMismaPersona
+ * 
+ * Recibe
+ * - `persona1`: Un objeto de la forma persona.
+ * - `persona2`: Otro objeto de la forma persona.
+ * 
+ * Retorna: 
+ * - un valor boolean ('true' o 'false'), indicando si persona1 y persona2 son la misma persona.
+ */
+function sonLaMismaPersona(persona1, persona2) {
+    mismaPersona1 = {
+        apellido: persona1["apellido"],
+        nombre: persona1["nombre"],
+        edad: persona1["edad"],
+        documento: persona1["documento"], 
+    };
+    mismaPersona2 = {
+        apellido: persona2["apellido"],
+        nombre: persona2["nombre"],
+        edad: persona2["edad"],
+        documento: persona2["documento"],
+    };
+    if(persona1.DNI==persona2.DNI){
+        return("Si son la misma persona");
+    }else{
+        return("No son la misma persona");
+    }
+}
+console.log("resultado sonLaMismaPersona: ", sonLaMismaPersona(
+    personaEjemplo,
+    { apellido: "Perez", nombre: "Juan", edad: 20, documento: 12345 }
+));
